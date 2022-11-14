ca56d2c5d6add17efa2010811c48cec0d8b35b47
Marcus Bitzl <marcus@bitzl.io>
2022-11-15 00:28:35 +0100

    Add image input

--- esrgan_dream/dream.py
+++ esrgan_dream/dream.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from pathlib import Path
 import random
 from typing import TextIO
 from basicsr.archs.rrdbnet_arch import RRDBNet
@@ -11,6 +13,28 @@ from esrgan_dream import ColorMode, IDGenerator, experiment_id
 import yaml
 
 ID_GENERATOR = IDGenerator()
+
+def create_upsampler(model_path: str, tile: str):
+        model = RRDBNet(
+            num_in_ch=3,
+            num_out_ch=3,
+            num_feat=64,
+            num_block=23,
+            num_grow_ch=32,
+            scale=4,
+        )
+        netscale = 4
+        return RealESRGANer(
+            scale=netscale,
+            model_path=model_path,
+            dni_weight=None,
+            model=model,
+            tile=tile,
+            tile_pad=10,
+            pre_pad=0,
+            half=True,
+            gpu_id=None,
+        )
 
 class Dream:
     def __init__(
@@ -46,27 +70,7 @@ class Dream:
 
     def dream(self, repeat_upscale, out: str, progress_callback: lambda: None):
         # Setup model. Don't do this in __init__ because this uses GPU memory.
-        model = RRDBNet(
-            num_in_ch=3,
-            num_out_ch=3,
-            num_feat=64,
-            num_block=23,
-            num_grow_ch=32,
-            scale=4,
-        )
-        netscale = 4
-        upsampler = RealESRGANer(
-            scale=netscale,
-            model_path=self.model_path,
-            dni_weight=None,
-            model=model,
-            tile=self.tile,
-            tile_pad=10,
-            pre_pad=0,
-            half=True,
-            gpu_id=None,
-        )
-
+        upsampler = create_upsampler(self.model_path, self.tile)
         img = self.blurry_noise()
         for iteration in range(
             repeat_upscale + 1
@@ -74,7 +78,7 @@ class Dream:
             if iteration > 0:
                 img, _ = upsampler.enhance(img, outscale=4)
             cv2.imwrite(
-                f"{out}/{self.id}_{self.tile}_{iteration:02d}.png", img
+                f"{out}/{self.id}_{iteration:02d}.png", img
             )
             progress_callback()
 
@@ -85,17 +89,17 @@ class Dream:
         match self.color_mode:
             case ColorMode.color:
                 print("Generate grayscale image")
-                img = rng.integers(max_value, size=(width, height, 3), dtype=np.uint8) + self.color_offset
+                img = rng.integers(max_value, size=(height, width, 3), dtype=np.uint8) + self.color_offset
                 if self.blur > 0:
                     img = cv2.blur(img, (self.blur, self.blur))
             case ColorMode.grayscale:
                 print("Generate grayscale image")
-                img = rng.integers(max_value, size=(width, height, 1), dtype=np.uint8) + self.color_offset
+                img = rng.integers(max_value, size=(height, width, 1), dtype=np.uint8) + self.color_offset
                 if self.blur > 0:
                     img = cv2.blur(img, (self.blur, self.blur))
             case ColorMode.black_and_white:
                 print("Generate black and white image")
-                img = rng.integers(255, size=(width, height, 1), dtype=np.uint8)
+                img = rng.integers(255, size=(height, width, 1), dtype=np.uint8)
                 img[img < 128] = 0
                 img[img >= 128] = 255
         return img
@@ -105,9 +109,88 @@ class Dream:
             "initial": {
                 "width": self.initial_width,
                 "height": self.initial_height,
-                "color_mode": self.color_mode,
+                "color_mode": repr(self.color_mode),
                 "blur": self.blur,
                 "color_offset": self.color_offset,
+            },
+            "seeds": {
+                "numpy": self.numpy_seed,
+                "torch": self.torch_seed,
+            },
+            "tile": self.tile,
+            "model_path": self.model_path
+        }
+        return yaml.dump(state)
+
+    def dump(self, fp) -> str:
+        fp.write(self.dumps())
+
+    @classmethod
+    def load(cls, fp: TextIO) -> "Dream":
+        doc = yaml.safe_load(fp)
+
+        initial = doc["initial"]
+        seeds = doc["seeds"]
+
+        return cls(
+            numpy_seed=seeds["numpy"],
+            torch_seed=seeds["torch"],
+            initial_width=initial["width"],
+            initial_height=initial["height"],
+            tile=doc["tile"],
+            color_mode=initial["color_mode"],
+            model_path=doc["model_path"],
+            blur=initial["blur"],
+            color_offset=initial.get("color_offset", 0),
+        )
+
+# @dataclass
+# class Seeds:
+#     numpy: int
+#     torch: int
+
+class DreamFromImage:
+    def __init__(
+        self,
+        image_path: Path,
+        numpy_seed: int,
+        torch_seed: int,
+        tile: int,
+        model_path: str,
+    ):
+        self.id = ID_GENERATOR.next()
+        self.image_path = image_path
+        self.tile = tile
+        self.model_path = model_path
+
+        if numpy_seed is None:
+            self.numpy_seed = random.randint(0, 2 ** 32 - 1)
+    
+        if torch_seed is not None:
+            torch.manual_seed(torch_seed)
+        self.torch_seed = torch_seed
+
+    def dream(self, repeat_upscale, out: str, progress_callback: lambda: None):
+        # Setup model. Don't do this in __init__ because this uses GPU memory.
+        upsampler = create_upsampler(self.model_path, self.tile)
+
+        img = cv2.imread(str(self.image_path))
+        for iteration in range(
+            repeat_upscale + 1
+        ):  # add one because in the first iteration we just save the original image
+            if iteration > 0:
+                img, _ = upsampler.enhance(img, outscale=4)
+            cv2.imwrite(
+                f"{out}/{self.id}_{self.tile}_{iteration:02d}.png", img
+            )
+            progress_callback()
+
+    def dumps(self) -> str:
+        state = {
+            "initial": {
+                "width": self.initial_width,
+                "height": self.initial_height,
+                "image_path": self.image_path,
             },
             "seeds": {
                 "numpy": self.numpy_seed,
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from pathlib import Path
 from rich.progress import Progress
 import typer
 from esrgan_dream import ColorMode
+from esrgan_dream import dream
 
-from esrgan_dream.dream import Dream
+from esrgan_dream.dream import Dream, DreamFromImage
 
 MAX_SEED = 2**32 - 1
 
@@ -50,25 +51,26 @@ def experiments(
     color_offset: int = typer.Option(
         0, help="Offset to make the image brighter or darker"
     ),
+    initial_width: int = typer.Option(16, help="Initial width of the image"),
+    initial_height: int = typer.Option(16, help="Initial height of the image"),
 ):
     out.mkdir(exist_ok=True, parents=True)
-    initial_width = 16
     numpy_seeds = [random.randint(0, MAX_SEED) for _ in range(experiments)]
     torch_seeds = [random.randint(0, MAX_SEED) for _ in range(experiments)]
 
     # setup experiments
     experiments = [
         Dream(
-                numpy_seed,
-                torch_seed,
-                initial_width,
-                initial_width,
-                tile,
-                color_mode,
-                model_path,
-                blur,
-                color_offset,
-            )
+            numpy_seed,
+            torch_seed,
+            initial_width,
+            initial_height,
+            tile,
+            color_mode,
+            model_path,
+            blur,
+            color_offset,
+        )
         for numpy_seed, torch_seed in zip(numpy_seeds, torch_seeds)
     ]
 
@@ -92,11 +94,49 @@ def experiments(
             experiment.dream(iterations, out, lambda: progress.advance(task))
     stop = time.time()
     n = len(experiments)
-    typer.secho(f"Finished {n} experiments in {stop - start:.2f} seconds ({(stop - start)/n:.2f} s/experiment))", fg=typer.colors.GREEN)
+    typer.secho(
+        f"Finished {n} experiments in {stop - start:.2f} seconds ({(stop - start)/n:.2f} s/experiment))",
+        fg=typer.colors.GREEN,
+    )
+
 
 @app.command()
-def from_image():
-    pass
+def from_image(
+    image_path: Path,
+    out: Path = typer.Option(Path("out"), help="Path to the output folder"),
+    model_path: str = typer.Option(
+        "weights/RealESRGAN_x4plus.pth", help="Path to the model file"
+    ),
+    iterations: int = typer.Option(3, help="Number of times to upscale the image"),
+    tile: int = typer.Option(512, help="Size for image tiles (0: no tiling)"),
+):
+    out.mkdir(exist_ok=True, parents=True)
+    numpy_seed = random.randint(0, MAX_SEED)
+    torch_seed = random.randint(0, MAX_SEED)
+
+    if image_path.is_file():
+        dreams = [DreamFromImage(image_path, numpy_seed, torch_seed, tile, model_path)]
+    else:
+        dreams = [
+            DreamFromImage(image, numpy_seed, torch_seed, tile, model_path)
+            for image in image_path.glob("*.png")
+        ]
+
+    with Progress() as progress:
+
+        def tracking_task(dream):
+            return progress.add_task(
+                f"{dream.id}: Upscaling {dream.image_path.name}",
+                total=iterations,
+            )
+
+        overall_progress = progress.add_task("[medium_purple1]Total progress", total=len(dreams) * iterations)
+        tasks = [(dream, tracking_task(dream)) for dream in dreams]
+        for dream, task in tasks:
+            def update():
+                progress.advance(overall_progress)
+                progress.advance(task)
+            dream.dream(iterations, out, update)
 
 if __name__ == "__main__":
     app()
